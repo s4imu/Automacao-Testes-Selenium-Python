e4028e420093cd6652567100b6e4c08a608f7803
Symon Barreto <contatosymon@gmail.com>
2023-07-06 22:35:21 -0400

    Using locators in pages

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,8 +6,12 @@ class LoginPage:
     # Class constructor
     def __init__(self):
         self.driver = conftest.driver
+        # Working with locators
+        self.username_field = (By.ID, "user-name")
+        self.password_field = (By.ID, "password")
+        self.login_button = (By.ID, "login-button")
 
     def login(self, user, password):
-        self.driver.find_element(By.ID, "user-name").send_keys(user)
-        self.driver.find_element(By.ID, "password").send_keys(password)
-        self.driver.find_element(By.ID, "login-button").click()
+        self.driver.find_element(*self.username_field).send_keys(user)
+        self.driver.find_element(*self.password_field).send_keys(password)
+        self.driver.find_element(*self.login_button).click()
