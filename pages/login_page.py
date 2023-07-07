51a1fb239d4e33cfc388e963ecfcd2d9aa6503e0
Symon Barreto <contatosymon@gmail.com>
2023-07-06 22:49:05 -0400

    Refactoring to use a Base Page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,9 @@
 import conftest
 from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
 
 # Required to be a class
-class LoginPage:
+class LoginPage(BasePage): # Heritage
     # Class constructor
     def __init__(self):
         self.driver = conftest.driver
@@ -12,6 +13,9 @@ class LoginPage:
         self.login_button = (By.ID, "login-button")
 
     def login(self, user, password):
-        self.driver.find_element(*self.username_field).send_keys(user)
-        self.driver.find_element(*self.password_field).send_keys(password)
-        self.driver.find_element(*self.login_button).click()
+        # self.driver.find_element(*self.username_field).send_keys(user)
+        # self.driver.find_element(*self.password_field).send_keys(password)
+        # self.driver.find_element(*self.login_button).click()
+        self.type(self.username_field, user)
+        self.type(self.password_field, password)
+        self.click(self.login_button)
